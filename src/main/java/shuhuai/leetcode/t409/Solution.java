0a38716bf10439457085309823b0464712faf1b7
殊怀丶 <lvzhihe_123@qq.com>
2023-05-19 10:36:23 +0800

    feat: 添加t405、t409、t412、t414、t415

---
+++ src/main/java/shuhuai/leetcode/t409/Solution.java
@@ -0,0 +1,15 @@
+package shuhuai.leetcode.t409;
+
+public class Solution {
+    public int longestPalindrome(String s) {
+        int[] letter = new int[128];
+        for(char c : s.toCharArray()) {
+            letter[c]++;
+        }
+        int count = 0;
+        for (int i : letter) {
+            count += (i % 2);
+        }
+        return count == 0 ? s.length() : (s.length() - count + 1);
+    }
+}
---
+++ src/main/java/shuhuai/leetcode/t414/Solution.java
@@ -0,0 +1,16 @@
+package shuhuai.leetcode.t414;
+
+import java.util.TreeSet;
+
+public class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> s = new TreeSet<>();
+        for (int num : nums) {
+            s.add(num);
+            if (s.size() > 3) {
+                s.remove(s.first());
+            }
+        }
+        return s.size() == 3 ? s.first() : s.last();
+    }
+}
