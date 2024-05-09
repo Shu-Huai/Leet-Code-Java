db4815d581f9a7ffd6a567113223eb15f4c3545f
殊怀丶 <lvzhihe_123@qq.com>
2024-05-10 00:33:26 +0800

    feat: 添加t575、t594、t598

---
+++ src/main/java/shuhuai/leetcode/problems/t594/Solution.java
@@ -0,0 +1,20 @@
+package shuhuai.leetcode.problems.t594;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int max = 0;
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                max = Math.max(max, map.get(key) + map.get(key + 1));
+            }
+        }
+        return max;
+    }
+}
