b5ecef56e61568f2c629734102cb2cb56a9076a4
殊怀丶 <lvzhihe_123@qq.com>
2024-08-28 07:57:45 +0800

    feat: 添加t643、t645、t661

---
+++ src/main/java/shuhuai/leetcode/problems/t645/Test.java
@@ -0,0 +1,21 @@
+package shuhuai.leetcode.problems.t645;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test {
+    public void test() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.isEmpty()) {
+            String[] str = input.split(" ");
+            int[] nums = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                nums[i] = Integer.parseInt(str[i]);
+            }
+            System.out.println(Arrays.toString(new Solution().findErrorNums(nums)));
+            input = sc.nextLine();
+        }
+    }
+}
