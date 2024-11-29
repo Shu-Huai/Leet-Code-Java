6d65ff1327e65ef0f84760d39ce0bae337875b9e
殊怀丶 <lvzhihe_123@qq.com>
2024-11-29 12:10:47 +0800

    feat: 添加t150、t151、t189、t198

---
+++ src/main/java/shuhuai/leetcode/problems/t150/Solution.java
@@ -0,0 +1,35 @@
+package shuhuai.leetcode.problems.t150;
+
+import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String item : tokens) {
+            switch (item) {
+                case "+" -> {
+                    Integer b = stack.pop();
+                    Integer a = stack.pop();
+                    stack.push(a + b);
+                }
+                case "-" -> {
+                    Integer b = stack.pop();
+                    Integer a = stack.pop();
+                    stack.push(a - b);
+                }
+                case "*" -> {
+                    Integer b = stack.pop();
+                    Integer a = stack.pop();
+                    stack.push(a * b);
+                }
+                case "/" -> {
+                    Integer b = stack.pop();
+                    Integer a = stack.pop();
+                    stack.push(a / b);
+                }
+                default -> stack.push(Integer.parseInt(item));
+            }
+        }
+        return stack.pop();
+    }
+}
