428e8bd8b93a50d7c7d69c02cc3a138f06d5393d
殊怀丶 <lvzhihe_123@qq.com>
2024-12-04 13:04:30 +0800

    feat: 添加t45、t125

---
+++ src/main/java/shuhuai/leetcode/problems/t45/Solution.java
@@ -0,0 +1,33 @@
+package shuhuai.leetcode.problems.t45;
+
+public class Solution {
+    public int jumpGreedy(int[] nums) {
+        int ans = 0;
+        int left = nums.length - 1;
+        while (left > 0) {
+            for (int i = 0; i < left; i++) {
+                if (i + nums[i] >= left) {
+                    left = i;
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int jumpClever(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
