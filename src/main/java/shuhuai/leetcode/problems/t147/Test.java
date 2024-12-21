9233fda87e50c8c78da5a797e733ab0a26b77265
殊怀丶 <lvzhihe_123@qq.com>
2024-12-21 19:08:22 +0800

    feat: 添加t147、t200、t219、t322

---
+++ src/main/java/shuhuai/leetcode/problems/t147/Test.java
@@ -0,0 +1,30 @@
+package shuhuai.leetcode.problems.t147;
+
+import shuhuai.leetcode.utils.ListHelper;
+import shuhuai.leetcode.utils.Printer;
+
+import java.util.Scanner;
+
+public class Test {
+    public void test() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.isEmpty()) {
+            String[] split = input.split(" ");
+            ListNode head;
+            try {
+                head = new ListHelper<>(ListNode.class).build(split);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            head = new Solution().insertionSortList(head);
+            try {
+                split = new ListHelper<>(ListNode.class).deBuild(head);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            new Printer<>().printArray(split);
+            input = sc.nextLine();
+        }
+    }
+}
