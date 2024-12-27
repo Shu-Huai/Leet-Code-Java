f12759e62fbd5691d6d236a2fd8c2515796915bf
殊怀丶 <lvzhihe_123@qq.com>
2024-12-27 19:34:58 +0800

    feat: 添加t80、t100

--- src/main/java/shuhuai/leetcode/problems/t100/Solution.java
+++ src/main/java/shuhuai/leetcode/problems/t100/Solution.java
@@ -2,8 +2,11 @@ package shuhuai.leetcode.problems.t100;
 
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
         if (p == null || q == null) {
-            return p == q;
+            return false;
         }
         if (p.val != q.val) {
             return false;
