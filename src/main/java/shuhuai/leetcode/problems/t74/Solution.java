fd808e30ed80d824de9d542cc878935f7dada329
殊怀丶 <lvzhihe_123@qq.com>
2025-01-09 00:42:04 +0800

    feat: 添加t15、t64、t74、t88

---
+++ src/main/java/shuhuai/leetcode/problems/t74/Solution.java
@@ -0,0 +1,47 @@
+package shuhuai.leetcode.problems.t74;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = 0;
+        while (i < matrix.length) {
+            if (matrix[i][0] == target) {
+                return true;
+            }
+            if (matrix[i][0] > target) {
+                break;
+            }
+            i++;
+        }
+        if (i == 0) {
+            return false;
+        }
+        i--;
+        while (j < matrix[0].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+
+    public boolean searchMatrixBinary(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix.length * matrix[0].length - 1;
+        while (left <= right) {
+            int mid = (left - right) / 2 + right;
+            int i = mid / matrix[0].length;
+            int j = mid % matrix[0].length;
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/shuhuai/leetcode/problems/t74/Test.java
@@ -0,0 +1,30 @@
+package shuhuai.leetcode.problems.t74;
+
+import shuhuai.leetcode.utils.Inputer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+    public void test(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.isEmpty()){
+            List<int[]> matrix = new ArrayList<>();
+            while (!input.isEmpty()){
+                int[] nums = new Inputer().stringToIntArray(input);
+                matrix.add(nums);
+                input = scanner.nextLine();
+            }
+            int[][] matrixArray = new int[matrix.size()][matrix.getFirst().length];
+            for (int i = 0; i < matrix.size(); i++) {
+                matrixArray[i] = matrix.get(i);
+            }
+            int target = Integer.parseInt(scanner.nextLine());
+            System.out.println(new Solution().searchMatrix(matrixArray, target));
+            System.out.println(new Solution().searchMatrixBinary(matrixArray, target));
+            input = scanner.nextLine();
+        }
+    }
+}
